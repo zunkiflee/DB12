d32917d876b4ccd2ed4bbbef6220db774c483b96
zunkiflee <50405937+zunkiflee@users.noreply.github.com>
2026-07-13 20:00:32 +0700

    now I know

--- main.py
+++ main.py
@@ -1,18 +1,2 @@
-print("Hello, World!!!!")
-
-# 2
-print("Data Science Bootcamp 12")
-
-# 3
-print("I'm learning Python github.")
-
-# 4
-print("I'm learning Python github. 2")
-
-
-# new function
-def add_two(num):
-    return num + 2
-
-
-print(add_two(5), add_two(10))
+#print("Hello, World!!!!")
+print("Now I know what git and github")
